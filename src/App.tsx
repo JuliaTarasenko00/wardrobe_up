f61b5ba501206edcef9a767ac17c8b2c14914e62
JuliaTarasenko00 <yuliiatarasenko.work@gmail.com>
2025-10-03 15:08:30 +0200

    created initial project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import { lazy, Suspense } from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-const Layout = lazy(() => import('./components/Layout/Layout'));
-
-// pages
-const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
-
-const router = createBrowserRouter([
-  {
-    path: '/wardrobe_up',
-    Component: Layout,
-    children: [{ index: true, Component: HomePage }],
-  },
-]);
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <Suspense fallback={<p>Loading....</p>}>
-      <RouterProvider router={router} />
-    </Suspense>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
